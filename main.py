a945b3b6e80e925035e6366430e87df270b1726d
wuj1021 <126232630+wuj1021@users.noreply.github.com>
2023-03-24 01:21:10 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# 讀取檔案
+with open('hw2_data.txt', 'r') as file:
+    # 建立空字典紀錄每個單字出現次數
+    word_count = {}
+    # 逐行讀取檔案
+    for line in file:
+        # 將每行轉為小寫並移除換行符號
+        words = line.lower().strip().split()
+        # 遍歷每個單字
+        for word in words:
+            # 若字典中沒有該單字，則新增一個key-value pair，value為1
+            if word not in word_count:
+                word_count[word] = 1
+            # 若字典中已經有該單字，則value加1
+            else:
+                word_count[word] += 1
+
+    # 統計不重複的英文字
+    unique_words = len(word_count)
+    print(f"總共有 {unique_words} 個不重複的英文字")
+    # 統計每個英文字出現次數和
+    print(word_count)
+    
